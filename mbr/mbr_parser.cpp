ae04970749af254e78923bbc7e4d2f0aab9d617b
Yejune Ko <koyejune0302@gmail.com>
2024-07-29 12:32:05 +0900

    Update mbr parser

--- mbr/mbr_parser.cpp
+++ mbr/mbr_parser.cpp
@@ -1,10 +1,50 @@
 // input file: disk image file
 // ex: mbr_parser ./mbr_128.dd
-
-// print all the partition information in the MBR section of the disk image file
-// output format: {filesystem type(ex: FAT32, NTFS, ... )} {start sector} {size}
-
 #include <stdio.h>
+
+
+int ebr_traversal(FILE* fp, unsigned int base_location, unsigned int offset){
+
+    unsigned char buf[512];
+    fseek(fp, (base_location + offset) * 512, SEEK_SET);
+    fread(buf, 1, 512, fp);
+
+    int i = 0x1be;
+
+     // check which filesystem type
+    char fs_type[10];
+    if(buf[i + 4] == 0x01){
+        sprintf(fs_type, "FAT12");
+    }else if(buf[i + 4] == 0x04){
+        sprintf(fs_type, "FAT16");
+    }else if(buf[i + 4] == 0x06){
+        sprintf(fs_type, "FAT16B");
+    }else if(buf[i + 4] == 0x07){
+        sprintf(fs_type, "NTFS");
+    }else if(buf[i + 4] == 0x0B){
+        sprintf(fs_type, "FAT32");
+    }else if(buf[i + 4] == 0x0C){
+        sprintf(fs_type, "FAT32X");
+    }else if(buf[i + 4] == 0x0E){
+        sprintf(fs_type, "FAT16X");
+    }else{
+        sprintf(fs_type, "Unknown");
+    }
+
+    // print the information
+    unsigned int start_sector = buf[i + 8] + (buf[i + 9] << 8) + (buf[i + 10] << 16) + (buf[i + 11] << 24);
+    unsigned int size = buf[i + 12] + (buf[i + 13] << 8) + (buf[i + 14] << 16) + (buf[i + 15] << 24);
+    printf("%s %u %u\n", fs_type, base_location + offset + start_sector, size);
+
+    i += 16;
+    if(buf[i + 4] == 0x05 || buf[i + 4] == 0x0F){
+        // NEXT EBR
+        ebr_traversal(fp, base_location, buf[i + 8] + (buf[i + 9] << 8) + (buf[i + 10] << 16) + (buf[i + 11] << 24));
+    }
+
+
+    return 0;
+}
 
 int read_mbr(const char* filename){
     FILE* fp = fopen(filename, "rb");
@@ -16,28 +56,11 @@ int read_mbr(const char* filename){
     unsigned char buf[512];
     fread(buf, 1, 512, fp);
 
-    // print mbr in hex
-    // for(int i = 0; i < 512; i++){
-    //     printf("%02X ", buf[i]);
-    //     if(i % 16 == 15){
-    //         printf("\n");
-    //     }
-    // }
-
-    // print pte in hex
-    // for(int i = 0; i < 4; i++){
-    //     for(int j = 0; j < 16; j++){
-    //         printf("%02X ", buf[0x1BE + i * 16 + j]);
-    //         if(j % 16 == 15){
-    //             printf("\n");
-    //         }
-    //     }
-    // }
-
     for(int i = 0x1BE; i < 0x1FE; i += 16){
         if(buf[i + 4] == 0x05 || buf[i + 4] == 0x0F){
-            // printf("EBR found\n");
-            return 0;
+            // EBR
+            ebr_traversal(fp, buf[i + 8] + (buf[i + 9] << 8) + (buf[i + 10] << 16) + (buf[i + 11] << 24), 0);
+            break;
         }
 
         // check which filesystem type
@@ -60,7 +83,6 @@ int read_mbr(const char* filename){
             sprintf(fs_type, "Unknown");
         }
 
-        // print the information
         unsigned int start_sector = buf[i + 8] + (buf[i + 9] << 8) + (buf[i + 10] << 16) + (buf[i + 11] << 24);
         unsigned int size = buf[i + 12] + (buf[i + 13] << 8) + (buf[i + 14] << 16) + (buf[i + 15] << 24);
         printf("%s %u %u\n", fs_type, start_sector, size);
